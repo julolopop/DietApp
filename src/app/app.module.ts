a049da1f2c45de77d79aa0bbf98cdc906a923d22
julolopop <julolopop@gmail.com>
2020-05-27 12:31:52 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './page/login/login.component';
+import { LoginTComponent } from './template/login-t/login-t.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginTComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/template/login-t/login-t.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-t',
+  templateUrl: './login-t.component.html',
+  styleUrls: ['./login-t.component.scss']
+})
+export class LoginTComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
